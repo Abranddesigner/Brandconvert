fa9b59b830159f372afc150a17d39baecd7051c0
Mr. Kamal Meena <ideogram682@gmail.com>
2025-07-28 12:11:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // script.js
-console.log("Pi Ecosystem Page Loaded");
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Pi Ecosystem Page Loaded");
+
+  // Optional: alert or future interaction
+  // alert("Welcome to the Pi Ecosystem");
+});
